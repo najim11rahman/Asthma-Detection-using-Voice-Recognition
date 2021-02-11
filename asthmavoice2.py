dd3e3e7d7b0230f25c4662fd2179e6094b0b2b74
Najim Rahman <44024640+najim11rahman@users.noreply.github.com>
2021-02-11 15:03:06 +0530

    Add files via upload

--- asthmavoice2.py
+++ asthmavoice2.py
@@ -2,25 +2,35 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.io import wavfile
 from python_speech_features import mfcc, logfbank
+import statistics
 
-
-frequency_sampling, audio_signal = wavfile.read("normal1.wav")
+frequency_sampling, audio_signal = wavfile.read("cough7.wav")
 
 audio_signal = audio_signal[:15000]
 
 features_mfcc = mfcc(audio_signal, frequency_sampling)
 
 filterbank_features = logfbank(audio_signal, frequency_sampling)
+
 sum = 0
-for i in range(len(filterbank_features)):
-    sum = sum + filterbank_features[i]
-    mean = sum/len(filterbank_features)
+for i in range(len(features_mfcc)):
+    sum = sum + features_mfcc[i]
+    mean = sum/len(features_mfcc)
 
-sum=0
-for i in range(len(mean)):
-    sum = sum + mean[i]
-    mean2 = sum/len(mean)
+variance = statistics.pvariance(mean)
 
-print(mean2)
-plt.hist(filterbank_features, bins=20)
-plt.show() 
+if(variance < 170):
+    print(variance)
+    print("LOW POSSIBILITY FOR ASTHMA")
+
+if((variance > 170) & (variance < 180)):
+    print(variance)
+    print("MEDIUM POSSIBILITY FOR ASTHMA")
+
+if((variance > 180) & (variance < 190)):
+    print(variance)
+    print("MEDIUM POSSIBILITY FOR ASTHMA")
+
+if(variance > 190):
+    print(variance)
+    print("HIGH POSSIBILITY FOR ASTHMA")
